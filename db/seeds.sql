6e874578bf41578de4251a769139da950fb14cfa
Hatice <haticeevci@gmail.com>
2021-02-16 08:52:39 -0500

    config and package added

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('THE IMPOSSIBLE');
+INSERT INTO burgers (burger_name) VALUES ('JUICY LUCY');
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('MINIS', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLUE', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie');
