43eee46140c417465fa2548c6d2c2a63552e52d4
Hatice <haticeevci@gmail.com>
2021-02-24 20:45:23 -0500

    connection error2

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-const connection ="";
+var connection;
 
 if (process.env.JAWSDB_URL) {
    connection = mysql.createConnection(process.env.JAWSDB_URL);
